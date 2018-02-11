e8ea88d734e37b2248d15df5917bb26863785ccc
I am not me <impeccabletiming@gmail.com>
2018-02-10 22:34:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A very simple mod for Minetest that creates colored letter blocks, similar to those babies play with. Don't look at this unless you want to feel better about your own art skills.
---
+++ init.lua
@@ -0,0 +1,155 @@
+minetest.register_node("letterblock:letterblock_a", {
+    description = "Letter A block",
+    tiles = {"letterblock_a.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_b", {
+    description = "Letter B block",
+    tiles = {"letterblock_b.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_c", {
+    description = "Letter C block",
+    tiles = {"letterblock_c.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_d", {
+    description = "Letter D block",
+    tiles = {"letterblock_d.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_e", {
+    description = "Letter E block",
+    tiles = {"letterblock_e.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_f", {
+    description = "Letter F block",
+    tiles = {"letterblock_f.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_g", {
+    description = "Letter G block",
+    tiles = {"letterblock_g.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_h", {
+    description = "Letter H block",
+    tiles = {"letterblock_h.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_i", {
+    description = "Letter I block",
+    tiles = {"letterblock_i.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_j", {
+    description = "Letter J block",
+    tiles = {"letterblock_j.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_k", {
+    description = "Letter K block",
+    tiles = {"letterblock_k.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_l", {
+    description = "Letter L block",
+    tiles = {"letterblock_l.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_m", {
+    description = "Letter M block",
+    tiles = {"letterblock_m.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_n", {
+    description = "Letter N block",
+    tiles = {"letterblock_n.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_o", {
+    description = "Letter O block",
+    tiles = {"letterblock_o.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_p", {
+    description = "Letter P block",
+    tiles = {"letterblock_p.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_q", {
+    description = "Letter Q block",
+    tiles = {"letterblock_q.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_r", {
+    description = "Letter R block",
+    tiles = {"letterblock_r.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_s", {
+    description = "Letter S block",
+    tiles = {"letterblock_s.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_t", {
+    description = "Letter T block",
+    tiles = {"letterblock_t.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_u", {
+    description = "Letter U block",
+    tiles = {"letterblock_u.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_v", {
+    description = "Letter V block",
+    tiles = {"letterblock_v.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_w", {
+    description = "Letter W block",
+    tiles = {"letterblock_w.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_x", {
+    description = "Letter X block",
+    tiles = {"letterblock_x.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_y", {
+    description = "Letter Y block",
+    tiles = {"letterblock_y.png",},
+    groups = {choppy = 3},
+})
+
+minetest.register_node("letterblock:letterblock_z", {
+    description = "Letter Z block",
+    tiles = {"letterblock_z.png",},
+    groups = {choppy = 3},
+})
